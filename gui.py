70ea69a1674391461b5fefcd3baf7cb59e28d142
Georgi Patrikov <patrikov1948@gmail.com>
2023-02-10 18:25:02 +0200

    trying urls

--- gui.py
+++ gui.py
@@ -7,9 +7,6 @@ from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import requests
-import pandas_datareader as pdr
-from sklearn.model_selection import train_test_split
-import datetime
 
 class StockPredictorApp(tk.Tk):
     def __init__(self):
@@ -45,11 +42,30 @@ class StockPredictorApp(tk.Tk):
         self.result_label = tk.Label(self, textvariable=self.result_text)
         self.result_label.grid(row=3, column=0, padx=10, pady=10, columnspan=3)
 
-        
-    def fetch(self):
-        url = self.url_entry.get()
+    def fetch_data_with_api_key(symbol, api_key):
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}"
         response = requests.get(url)
-        self.data = response.json()
+    
+        if response.status_code == 200:
+            data = response.json()
+            time_series = data["Time Series (Daily)"]
+            stock_data = []
+            for date, values in time_series.items():
+                stock_data.append({
+                 "date": date,
+                 "open": float(values["1. open"]),
+                 "high": float(values["2. high"]),
+                 "low": float(values["3. low"]),
+                 "close": float(values["4. close"]),
+                 "volume": int(values["5. volume"])
+            })
+            df = pd.DataFrame(stock_data)
+            df["date"] = pd.to_datetime(df["date"])
+            df.set_index("date", inplace=True)
+            return df
+        else:
+         print("Failed to fetch data with the given API key")
+         return None
 
     def browse_file(self):
         self.file_path = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("CSV files", "*.csv"), ("all files", "*.*")))
@@ -65,29 +81,43 @@ class StockPredictorApp(tk.Tk):
     def predict(self):
         while True:
             file_path = self.file_entry.get()
-            
-            if file_path == "":
-                messagebox.showerror("Error", "Please select a file")
-                return
-        # Load the stock data using pandas
-            try:
+            symbol = self.symbol_entry.get()
+            api_key = self.api_key_entry.get()
+        
+            if file_path == "" and symbol == "":
+             messagebox.showerror("Error", "Please select a file or enter a symbol")
+             return
+            elif file_path != "":
+            # Load the stock data from a local file
+             try:
                 stock_data = pd.read_csv(file_path)
-            except FileNotFoundError:
+             except FileNotFoundError:
                 messagebox.showerror("Error", "File not found")
                 self.file_entry.delete(0, tk.END)
                 continue
+            if symbol != "":
+             API_KEY = "78509L5D6INZ29LP"
+             url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}"
+             response = requests.get(url)
+             data = response.json()
+             if "Error Message" in data:
+                 messagebox.showerror("Error", data["Error Message"])
+                 return
+            stock_data = pd.DataFrame(data["Time Series (Daily)"]).T
+            stock_data.index = pd.to_datetime(stock_data.index)
+            stock_data.sort_index(ascending=True, inplace=True)
 
         # Prepare the data for prediction
-            X = np.array(range(0, len(stock_data))).reshape(-1, 1)
+            x = np.array(range(0, len(stock_data))).reshape(-1, 1) 
             y = stock_data["Close"].values
 
         # Train the SVM model
             svm = SVR(kernel="linear", C=1, epsilon=0.1)
-            svm.fit(X, y)
+            svm.fit(x, y)
 
         # Train the Linear Regression model
             linear_regression = LinearRegression()
-            linear_regression.fit(X, y)
+            linear_regression.fit(x, y)
 
         # Use the models to make a prediction
             svm_prediction = svm.predict([[len(stock_data)]])[0]
@@ -95,22 +125,16 @@ class StockPredictorApp(tk.Tk):
 
         # Display the result in the GUI
             self.result_text.set(f"SVM: {svm_prediction}, Linear Regression: {linear_regression_prediction}")
-
+            
+            plt.plot()
             plt.plot(stock_data['Close'], label='Actual')
-            plt.plot(svm_prediction, label='SVM Prediction')
-            plt.plot(linear_regression_prediction, label='Linear Regression Prediction')
             plt.legend()
-           
             
-           # data = pd.read_csv(file_path)
-           # dates = data['Date'].tolist()
-           # close_prices = data['Close'].tolist()
+            ax = plt.subplot()
+            ax.plot(len(stock_data)+1, svm_prediction, 'ro', label='SVM Prediction')
+            ax.plot(len(stock_data)+1, linear_regression_prediction, 'go', label='Linear Regression Prediction')
+            ax.legend()  
             
-           # ax=plt.subplot()
-           # ax.plot(dates, close_prices)
-           # ax.set_title("Stock Price over time")
-           # plt.xticks(rotation=45)
-           # plt.tight_layout()
             plt.show()
             
             self.file_entry.delete(0, tk.END)
